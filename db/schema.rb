ad37aa83f0a9463d4fc4bfb38ed6384049ee6c2c
Sassa Kitka <sassa.kitka@gmail.com>
2016-10-12 15:41:46 -0700

    added medium model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161012224125) do
+
+  create_table "media", force: :cascade do |t|
+    t.string   "title"
+    t.string   "maker"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
