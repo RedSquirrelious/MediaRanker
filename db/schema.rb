554e1a55e13480e2815d341bc4e41f164d31cd9f
Sassa Kitka <sassa.kitka@gmail.com>
2016-10-12 15:42:28 -0700

    added vote model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012224125) do
+ActiveRecord::Schema.define(version: 20161012224212) do
 
   create_table "media", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20161012224125) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "medium_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
