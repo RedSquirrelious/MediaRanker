29f612e4e0939f882dd6f0efb7762ebd5c9a29e5
Sassa Kitka <sassa.kitka@gmail.com>
2016-10-12 17:45:31 -0700

    added validation testing

--- test/models/medium_test.rb
+++ test/models/medium_test.rb
@@ -4,4 +4,17 @@ class MediumTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+#validations
+  test 'A Medium Must Have a Title' do
+  	assert media(:flood).valid?
+  end
+
+  test 'A Medium Must Have a Maker' do
+  	assert_equal( media(:flood).maker, 'They Might Be Giants')
+  end
+
+  test 'A Medium Must Have a Description' do
+     assert_not_nil( media(:flood), :description)
+  end
+
 end
