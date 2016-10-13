1fadf546f23c4c8225863b575e3a4e4e22be2da0
Sassa Kitka <sassa.kitka@gmail.com>
2016-10-12 17:42:41 -0700

    added validations"

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,3 +1,7 @@
 class Medium < ActiveRecord::Base
 	has_many :votes
+	validates :title, presence: true
+	validates :maker, presence: true
+	validates :description, presence: true
+	validates :kind, presence: true
 end
