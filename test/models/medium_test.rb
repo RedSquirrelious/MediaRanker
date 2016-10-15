369b0a8e7c7f7f08961b43f17ad5f3d89b708104
Sassa Kitka <sassa.kitka@gmail.com>
2016-10-15 16:29:13 -0700

    added ideas for further tests

--- test/models/medium_test.rb
+++ test/models/medium_test.rb
@@ -9,12 +9,29 @@ class MediumTest < ActiveSupport::TestCase
   	assert media(:flood).valid?
   end
 
+# raise error if no title
+
+#   title must be a string
+#   maker name must be a string
+#   description must be a string
+
+# title cannot be just spaces
+# maker name cannot be just spaces
+# description cannot be just spaces
+
+
+
+
   test 'A Medium Must Have a Maker' do
   	assert_equal( media(:flood).maker, 'They Might Be Giants')
   end
 
+  raise error if no maker
+
   test 'A Medium Must Have a Description' do
      assert_not_nil( media(:flood), :description)
   end
+
+  raise error if no description
 
 end
